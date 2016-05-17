810901da26d233d9e1e4f16cf4fc4248295e58c3
escuelita <escuelita>
2016-05-17 11:03:12 -0300

    CV con JSON

---
+++ Pablo/Ejercicio 8/ejer8.js
@@ -0,0 +1,5 @@
+var text = {"nombre":"Pablo Pirovano Varela", "job":"Freelance Procrastinator", "casa":"Castelar, Buenos Aires, ARG"};
+//var json = {'nombre': ''}//
+$($('h1')[0]).html(text.nombre);
+$($('h3')[0]).html(text.job);
+$($('p')[0]).html(text.casa);
