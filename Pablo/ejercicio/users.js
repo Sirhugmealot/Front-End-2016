c8e4393d047de8f327bfc6060d9c840268ca23b9
escuelita <escuelita>
2016-05-19 15:28:05 -0300

    esto es server

--- Pablo/ejercicio/users.js
+++ Pablo/ejercicio/users.js
@@ -3,10 +3,12 @@ module.exports = function(server){ //siempre module.exports
 		var usuarios = [
 		{
 			name:'pablo',
-			email:'pablo@pirovano.me'
+			email:'pablo@pirovano.me',
+			comment:''
 		},{
 			name:'agustin',
-			email:'agustin.diaz@gl.com'
+			email:'agustin.diaz@gl.com',
+			comment:''
 		}];
 		this.getUser = function(req,res,next){ //leer
 			var userId = req.params.id;
@@ -15,7 +17,7 @@ module.exports = function(server){ //siempre module.exports
 			}
 			else {res.send(404,'la cagaste mi wuen')}
 			return next();
-		};
+		}
 
 		this.editUser = function(req, res, next){ //modificar
 			var userId = req.params.id,
@@ -26,7 +28,8 @@ module.exports = function(server){ //siempre module.exports
 			}
 			else {res.send(404,'la cagaste mi wuen')}
 			return next();
-		};
+		}
+
 		this.addUser = function(req, res, next){ //añadir
 			var user = {};
        			user.name = req.params.name;
@@ -40,22 +43,66 @@ module.exports = function(server){ //siempre module.exports
       		}
       		else {res.send(404,'la cagaste mi wuen')}
 			return next();
-		};
+		}
+
+		this.deleteUser = function(req, res, next){ //añadir
+			var userId = req.params.id;
+       			
+      			if (usuarios[userId]){
+      				delete(usuarios[userId]);
+      				usuarios = usuarios.filter( function(usuarios) { return !!usuarios; });
+       				res.send(200,'elemento eliminado');
+      		}
+      		else {res.send(404,'la cagaste mi wuen')}
+			return next();
+		}
+
+		this.commentUser = function(req, res, next){ //modificar
+			var userId = req.params.id,
+				newComment = req.params.comment;
+				if (usuarios[userId]){
+					usuarios[userId].comment = newComment; //agrega comentario
+				res.send(200, usuarios[userId]); //devuelve el usuario
+			}
+			else {res.send(404,'la cagaste mi wuen')}
+			return next();
+		}
+		this.allUser = function(req,res,next){ //leer
+			res.send(200, usuarios); //devuelve el usuario si encuentra un usuario
+			return next();
+		}
+
+
 	};
 	var User = new UsersModel();
 	server.get({
-			path: '/users/:id',
-			version:'1.0.0'
+		path: '/users/:id',
+		version:'1.0.0'
 	}, User.getUser);
 
-	server.post({
+	server.get({
+		path: '/users',
+		version:'1.0.0'
+	}, User.allUser);
+
+	 server.post({
+        path: '/users/:id',
+        version:'1.0.0'
+    }, User.addUser);
+
+	server.del({
+		path: '/users/:id',
+		version:'1.0.0'
+	}, User.deleteUser);
+
+	server.put({
 		path: '/users/:id',
 		version:'1.0.0'
 	}, User.editUser);
 
 	server.put({
-		path: '/users/:id',
+		path: '/com/:id',
 		version:'1.0.0'
-	}, User.addUser)
+	}, User.commentUser);
 };
 
