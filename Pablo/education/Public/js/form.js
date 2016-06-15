151949fbff85f9665e3cd8f12e7f5111a200704f
escuelita <escuelita>
2016-06-15 16:20:24 -0300

    funcionaaaaaaaa

--- Pablo/education/Public/js/form.js
+++ Pablo/education/Public/js/form.js
@@ -1,12 +1,12 @@
 //var ;
 var todo,
 	id,
-	users,
-	educationes=[],
-	summary=[];
+	educc=[],
+	users=[];
 
 $(function(){
 	var form = $('container'),
+		i=-1,
 		templateContainer = $('#template');
 		templateContainer.load('/todo/todotemplate.html',function(){
 			botoooness = templateContainer.html();
@@ -22,22 +22,41 @@ function leBase(){
 
 		param = paramarr[1];
 		id=param;
+
 	form.append('<div id="ess">'+'</div>');
 	$('#ess').append('<form id="avv">'+'</form>');
+	getId(id);
+	
+}
+
+function getId(id){
 	$.ajax({
 		url:'http://connectedin.herokuapp.com/person/'+id,
 		method:'GET',
+		contentType: 'application/json',
 		success:function(data){
+			var l,
+				summary={},
+				b;
+
 				users = data.education;
-			for(var j=0;j<users.length;j++){
-				var	summary = users[j].val().split('\n'),
-					title = summary.splice(0,1);
-				for (var i = 0; i<summary.length;i++) {
-					if(i==0){
-						$('#avv').append('<br>'+title);
+				summary = users;
+			console.log(data.education.length);
+			for (var i = 0; i<summary.length;i++) {
+				$('#avv').append('<br>');
+				b=0;
+				l=1;
+				while(summary[i].edu2[l]){
+					if(b==0){
+						b=1;
+						$('#avv').append('<br>'+summary[i].edu1);
+
 					}
-					var booton='<br><span id="todolisto">'+summary[i]+'</span>';
-					$('#avv').append(booton);
+					else{  
+						var booton='<br><span id="todolisto">'+summary[i].edu2[l]+'</span>';
+						$('#avv').append(booton);
+					l++;
+					}
 				}
 			}
 		}
@@ -55,34 +74,49 @@ function toDo(){
 		
 	function agregarTarea(){
 		var title = $('#title').val(),
+			educationa={},
+			summary={},
 			todo = $('#inpt1').val().split('\n'); // variable a guardar
-			summary[0] = title;
+			//summary[0] = title;
+			console.log(educc, educationa, todo);
 
 
 		if(todo){
-			if(todo!=""){
-				// Muestro el education
-				for(var j=0;j<todo.length;j++){
-					summary[j+1]=todo[j];
-					var booton='<br><span id="todolisto">'+todo[j]+'</span>';
-					$('#ess').append('<div id="jss">'+'</div');
-					if(j==0){
-						jss=$('#jss');
-						jss.append('<br>'+title);
-					}
-					jss.append(booton);
+			if(todo[0]!=""){
+				if(todo.length!=0){
+					// Muestro el education
+					for(var j=0;j<todo.length;j++){
+						summary[j+1]=todo[j];
+						var booton='<br><span>'+todo[j]+'</span>';
+						$('#ess').append('<div id="jss">'+'</div');
+						if(j==0){
+							jss=$('#jss');
+							//jss.append('<br>'+title);
+							
+						}
+						//jss.append(booton);
+					}	
+					educationa.edu2=summary;
+					educationa.edu1=title;
+					educc.push(educationa);
+
+					var user={education:educc};
+					console.log(educc, educationa, todo);
+					$.ajax({ 
+						url:'http://connectedin.herokuapp.com/person/'+id,
+						method: 'PUT',
+						data: JSON.stringify(user),
+						contentType: 'application/json',
+						success: function(data){
+							alert('User Added');
+						}
+					});
+
+
 				}			
 			}
 		}
-		$.ajax({ 
-			url:'http://connectedin.herokuapp.com/person',
-			method: 'POST',
-			data: JSON.stringify(user),
-			contentType: 'application/json',
-			success: function(data){
-				alert('User Added');
-			}
-		});
+
 		return false;
 	}
 
